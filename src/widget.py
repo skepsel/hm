413bd3efe0bb8a013abefea0bf1e06eef58697ab
unknown <you@example.com>
2025-01-30 16:11:12 +0300

    update module widget

--- src/widget.py
+++ src/widget.py
@@ -16,7 +16,16 @@ def mask_account_card(card_inf: str) -> str:
         return card_inf[:index-1]+' '+masks.get_mask_card_number(card_inf[index:])
 
 
+def get_date(date:str) -> str:
+    '''Функция которая принимает на вход строку с датой в формате
+       "2024-03-11T02:26:18.671407"
+       и возвращает строку с датой в формате "ДД.ММ.ГГГГ" '''
+    year,month,day = date[:10].split('-')
+    return f'{day}.{month}.{year}'
+
+
 print(mask_account_card('Maestro 1596837868705199'))
 print(mask_account_card('Visa Classic 6831982476737658'))
 print(mask_account_card('Visa Gold 5999414228426353'))
 print(mask_account_card('Счет 73654108430135874305'))
+print(get_date('2024-03-11T02:26:18.671407'))
