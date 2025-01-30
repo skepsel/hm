9f661893d6fb236a80ae9e3c28dfab77615610cf
unknown <you@example.com>
2025-01-30 16:06:55 +0300

    new module widget

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+from src import masks
+
+
+def mask_account_card(card_inf: str) -> str:
+    '''Обрабатывает информацию как о картах, так и о счетах
+    Возвращает строку с замаскированным номером'''
+    index = 0
+    for i in card_inf:
+        if i.isdigit():
+            break
+        else:
+            index += 1
+    if card_inf[:index-1] == 'Счет':
+        return card_inf[:index-1]+' '+masks.get_mask_account(card_inf[index:])
+    else:
+        return card_inf[:index-1]+' '+masks.get_mask_card_number(card_inf[index:])
+
+
+print(mask_account_card('Maestro 1596837868705199'))
+print(mask_account_card('Visa Classic 6831982476737658'))
+print(mask_account_card('Visa Gold 5999414228426353'))
+print(mask_account_card('Счет 73654108430135874305'))
