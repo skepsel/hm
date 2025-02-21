4b4fd82ec35ed1d444d93fb86aab6c5959add2f4
Perkhalyuk.K <kyromaku@gmail.com>
2025-02-21 12:19:33 +0300

    update

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,4 @@ def filter_by_state(slovari: list[dict], state: str = "EXECUTED"):
 
 def sort_by_date(slovari: list[dict], reverse: bool = True):
     return sorted(slovari, key=lambda time: time["date"], reverse=(reverse == True))
+
