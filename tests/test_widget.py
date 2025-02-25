74eb42e08997f3ac65ae15b94b30905af6bdd7a8
Perkhalyuk.K <kyromaku@gmail.com>
2025-02-25 11:12:05 +0300

    update 2.0

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,19 @@
-from src.widget import get_date
+import pytest
+from src.widget import get_date, mask_account_card
 
 
+@pytest.fixture
+def get_dates() -> str:
+    return "2019-07-03T18:35:29.512364"
+
+
+@pytest.mark.parametrize(
+    "data, result",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79  6361"),
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79 **** 6361"),
+    ],
+)
 def test_mask_account_card(data: str, result: str) -> None:
     assert mask_account_card(data) == result
 
@@ -9,9 +22,9 @@ def test_get_date(date_input: str) -> None:
     assert get_date(date_input) == "03.07.2019"
 
 
-def test_get_date(get_dates: str) -> None:
-    assert get_date(get_dates) == "03.07.2019"
-
-
 def test_get_date_2(date_input: str) -> None:
     assert get_date(date_input) == "05.11.2015"
+
+
+def test_get_date(get_dates: str) -> None:
+    assert get_date(get_dates) == "03.07.2019"
