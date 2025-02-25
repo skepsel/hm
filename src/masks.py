850be5347a8045c2ed9b2346a9a4ade249614369
Perkhalyuk.K <kyromaku@gmail.com>
2025-02-25 11:20:34 +0300

    update 2.0

--- src/masks.py
+++ src/masks.py
@@ -15,3 +15,21 @@ def get_mask_account(account_number: str) -> str:
 
 
 print(get_mask_account("234"))
+
+
+def get_mask_card_number(card_number: int) -> str:
+
+
+    def get_mask_card_number(card_number: str) -> str:
+        """Маскирует номер банковской карты по правилу XXXX XX** **** XXXX."""
+        card_number_str = str(card_number)
+        return f"{card_number_str[:4]} {card_number_str[4:6]} **** {card_number_str[-4:]}"
+
+
+def get_mask_account(account_number: int) -> str:
+
+
+    def get_mask_account(account_number: str) -> str:
+        """Маскирует номер банковского счета по правилу **XXXX."""
+        account_number_str = str(account_number)
+        return "" + account_number_str[-4:]
