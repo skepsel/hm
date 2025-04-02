4d931f86621c9fc18e9e16c896886f64e2471b2d
Perkhalyuk.K <kyromaku@gmail.com>
2025-04-02 16:20:32 +0300

    12.2

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,3 @@
-
 import os
 
 import requests
@@ -16,7 +15,7 @@ def get_transaction_amount(transaction_info, currency="RUB"):
         API_KEY = os.getenv("API_KEY")
         headers = {"apikey": f"{API_KEY}"}
         response = requests.get(url, headers=headers)
-        return response.json().get('result')
+        return response.json().get("result")
 
 
 transaction = {
